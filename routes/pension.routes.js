cd9baf8ae3d3d08d6fe22c03b4d18baa319c1a93
Avishka777 <a.rathnakumara777@gmail.com>
2026-01-27 16:24:53 +0530

    Developed Function for Govi Pension

---
+++ routes/pension.routes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../Middlewares/auth.middleware");
+const pensionRequestController = require("../end-point/pension-ep");
+const upload = require("../Middlewares/multer.middleware");
+
+// Check Pension Request Status by NIC
+router.post(
+  "/pension-request/check-status-by-nic",
+  auth,
+  pensionRequestController.checkPensionRequestStatusByNIC,
+);
+
+// Submit Pension Request
+router.post(
+  "/pension-request/submit",
+  upload.fields([
+    { name: "nicFront", maxCount: 1 },
+    { name: "nicBack", maxCount: 1 },
+    { name: "sucNicFront", maxCount: 1 },
+    { name: "sucNicBack", maxCount: 1 },
+    { name: "birthCrtFront", maxCount: 1 },
+    { name: "birthCrtBack", maxCount: 1 },
+  ]),
+  pensionRequestController.submitPensionRequest,
+);
+
+module.exports = router;
